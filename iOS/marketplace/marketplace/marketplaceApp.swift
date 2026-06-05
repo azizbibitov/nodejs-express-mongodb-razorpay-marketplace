4c7c47942584ef9c3ae2e0ccfea607e7dc63e27a
Aziz Bibitov <azizbibitov11@gmail.com>
2026-06-06 03:33:00 +0500

    Add iOS project to repository

---
+++ iOS/marketplace/marketplace/marketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketplaceApp.swift
+//  marketplace
+//
+//  Created by Aziz Bibitov on 06.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct marketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/marketplace/marketplaceTests/marketplaceTests.swift
@@ -0,0 +1,16 @@
+//
+//  marketplaceTests.swift
+//  marketplaceTests
+//
+//  Created by Aziz Bibitov on 06.06.2026.
+//
+
+import Testing
+
+struct marketplaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
